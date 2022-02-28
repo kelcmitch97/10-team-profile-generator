f91729a9f9e94599bf1f5f94efc952025d8acc67
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-02-28 18:31:58 -0330

    First employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,10 +2,11 @@
 // const { test } = require('picomatch');
 const Employee = require('../lib/Employee');
 
-test('creates an employee class', () => {
-    const employee = new Employee('Tim');
+test('creates an employee object', () => {
+    const employee = new Employee('Tim', 9 , 'tim@gmail.com');
 
-    expect(employee.name).toBe('Tim');
+    expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).to
-})
+    expect(employee.email).toEqual(expect.any(String));
+});
+
